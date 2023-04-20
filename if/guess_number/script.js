27f32dcea2fc1c9564c0ca83f4796480f938be8d
Priymak Vladimir <priymakvl@gmail.com>
2023-04-20 16:56:19 +0300

    change guess number

--- if/guess_number/script.js
+++ if/guess_number/script.js
@@ -1,30 +1,28 @@
-let input = document.querySelector(".field__form__input");
-let btn = document.querySelector(".field__form__btn");
-let check = document.querySelector(".result__check__out");
-let help = document.querySelector(".result__help__out");
-let count = document.querySelector(".result__count__out");
-
+let input = document.querySelector("#number");
+let btn = document.querySelector(".btn");
+let check = document.querySelector(".result");
+let help = document.querySelector(".help");
+let count = document.querySelector(".count");
+let maxItem = 10;
 let item = 0;
 let randNum = 1 + Math.floor(Math.random() * 100);
 let userNum;
 
-btn.onclick = function (evt) {
-  evt.preventDefault();
+btn.onclick = function () {
+  if (item >= maxItem) return alert('Вы проиграли')
+  count.textContent = ++item;
   userNum = input.value;
   if (userNum > randNum) {
     check.textContent = "Пока что не угадали";
     help.textContent = "Многовато будет";
-    item++;
-    count.textContent = item;
   } else if (userNum < randNum) {
     check.textContent = "Пока что не угадали";
     help.textContent = "Маловато будет";
-    item++;
-    count.textContent = item;
   } else {
     check.textContent = "Поздравляю! Вы угадали число";
     help.textContent = "В самый раз";
-    item++;
-    count.textContent = item;
+    item = 0;
+    randNum = 1 + Math.floor(Math.random() * 100);
+    alert('Вы победили!! Можете ещё сыграть');
   }
 };
