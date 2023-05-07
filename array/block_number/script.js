c7302a6b9a7e58ad7d62ff005479961af8c2983e
Priymak Vladimir <priymakvl@gmail.com>
2023-05-07 08:32:28 +0300

    change block number

--- array/block_number/script.js
+++ array/block_number/script.js
@@ -4,9 +4,17 @@ const blocks = document.querySelectorAll('.block');
 select.onchange = setNum;
 
 function setNum() {
-	let num = this.value;
-	let index = num - 1;
-	blocks[index].innerText = num;
-	blocks[index].style.fontSize = '2rem';
+	if (this.value == 'reset') {
+		blocks.forEach(block => block.innerText = 'Блок');
+	} 
+	else {
+		let num = this.value;
+		let index = num - 1;
+		blocks[index].innerText = num;
+	}
 }
+
+
+
+
 
