94ba1979db875551996dd8cdec7de48c89435286
Priymak Vladimir <priymakvl@gmail.com>
2023-06-06 19:39:36 +0300

    change analog clock date folder

--- date/analog_clock/script.js
+++ date/analog_clock/script.js
@@ -1,17 +1,3 @@
-// setInterval(() => {
-//     var d = new Date();
-//     var htime = d.getHours();
-//     var mtime = d.getMinutes();
-//     var stime = d.getSeconds();
-//     var hrotation = 30*htime + mtime/2;
-//     var mrotation = 6*mtime;
-//     var srotation = 6*stime;
-
-//     hour.style.transform = `rotate(${hrotation}deg)`;
-//     minute.style.transform = `rotate(${mrotation}deg)`;
-//     second.style.transform = `rotate(${srotation}deg)`;
-// }, 1000);
-
 function clock() {
     let now= new Date();
     let hr = now.getHours();
@@ -21,14 +7,18 @@ function clock() {
     rotation(hr, min, sec);
 }
 
+setInterval(clock, 1000);
+
 function rotation(hr, min, sec) {
     let hrRotation= 30 * hr + min / 2;
     let minRotation = 6 * min;
     let secRotation = 6 * sec;
 
-    hour.style.transform = `rotate(${hrRotation}deg)`;
-    minute.style.transform = `rotate(${minRotation}deg)`;
-    second.style.transform = `rotate(${secRotation}deg)`;
+    hour.style.transform = `translate(-50%) rotate(${hrRotation}deg)`;
+    minute.style.transform = `translate(-50%) rotate(${minRotation}deg)`;
+    second.style.transform = `translate(-50%) rotate(${secRotation}deg)`;
 }
 
-setInterval(clock, 1000);
+
+
+
