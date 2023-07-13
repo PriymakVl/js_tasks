c2762981ba8c8509ce18cb9874115fdbd9d9c3e4
Priymak Vladimir <PriymakVl@gmail.com>
2023-07-13 10:45:16 +0300

    add filter tabs

---
+++ array/filter-tabs/script.js
@@ -0,0 +1,37 @@
+const allBtn = document.querySelector('.buttons #all');
+const carBtn = document.querySelector('.buttons #cars');
+const natureBtn = document.querySelector('.buttons #nature');
+const allImages = document.querySelectorAll('.content img');
+const modal = document.querySelector('.modal');
+
+// Displays all the images.
+allBtn.addEventListener('click', () => showImages());
+
+// Displays all the car images.
+carBtn.addEventListener('click', () => showImages('car'));
+
+// Displays all the nature images.
+natureBtn.addEventListener('click', () => showImages('nature'));
+
+function showImages(className) {
+    if (!className) return allImages.forEach(img => img.style.display = 'block');
+    allImages.forEach(img => {
+        if (img.className == className) img.style.display = 'block';
+        else img.style.display = 'none';
+    })
+}
+
+// Showing a pop when clicking the images.
+allImages.forEach(img => {
+    img.addEventListener('click', (e)=>{
+        modal.style.display = 'flex';
+        document.querySelector('.modal img')  = e.target.src;
+    });
+});
+
+// closing the Popup.
+modal.addEventListener('click', (e) => {
+    const modalImg = modal.querySelector('img');
+    if(e.target != modalImg) modal.style.display = 'none';
+});
+
