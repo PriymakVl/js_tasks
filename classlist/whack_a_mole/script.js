e55ceb7f1ca16102d6b13ca6b9380e5b43d2d7e8
Priymak Vladimir <PriymakVl@gmail.com>
2024-03-19 08:34:41 +0200

    add game wack a move -classlist

---
+++ classlist/whack_a_mole/script.js
@@ -0,0 +1,58 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const startBtn = document.querySelector('#start');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+startBtn.addEventListener('click', startGame);
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+
+  if(hole === lastHole) {
+    console.log('Same one');
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(800, 1600);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if(!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  startBtn.style.visibility = 'hidden';
+  peep();
+  setTimeout(() => {
+    timeUp = true;
+    button.innerHTML = 'Try again?'
+    button.style.visibility = 'visible';
+  }, 10000);
+}
+
+function bonk(e) {
+  if(!e.isTrusted) return;
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
