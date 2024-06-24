227d643c9233617422abed38e17da8de2ec52f9e
Priymak Vladimir <PriymakVl@gmail.com>
2024-06-24 20:41:20 +0300

    change task click counter

--- event/counter_click_btn/script.js
+++ event/counter_click_btn/script.js
@@ -4,10 +4,10 @@ const plusEl = document.querySelector('.plus');
 const minusEl = document.querySelector('.minus');
 const resetEl = document.querySelector('.reset');
 
-// plusEl.onclick = function() { value.innerText = ++num; }
-// minusEl.onclick = function() { value.innerText = --num; }
-// resetEl.onclick = function() { value.innerText = 0; }
+plusEl.onclick = function() { value.innerText = ++num; }
+minusEl.onclick = function() { value.innerText = --num; }
+resetEl.onclick = function() { value.innerText = num = 0; }
 
-plusEl.onclick = () =>  value.innerText = ++num;
-minusEl.onclick = () => value.innerText = --num;
-resetEl.onclick = () => value.innerText = 0;
+// plusEl.onclick = () =>  value.innerText = ++num;
+// minusEl.onclick = () => value.innerText = --num;
+// resetEl.onclick = () => value.innerText = num = 0;
