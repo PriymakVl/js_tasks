c56eeb3187c9de0e6322a73cb579e4ce65a95cee
Priymak Vladimir <PriymakVl@gmail.com>
2025-10-08 09:34:41 +0300

    change task filter tabs

--- array/filter-tabs/script.js
+++ array/filter-tabs/script.js
@@ -1,11 +1,11 @@
 const allBtn = document.querySelector('.buttons #all');
 const carBtn = document.querySelector('.buttons #cars');
 const natureBtn = document.querySelector('.buttons #nature');
-const allImages = document.querySelectorAll('.content img');
-const modal = document.querySelector('.modal');
+const images = document.querySelectorAll('.content img');
+
 
 // Displays all the images.
-allBtn.addEventListener('click', () => showImages());
+allBtn.addEventListener('click', () => images.forEach(img => img.style.display = 'block'));
 
 // Displays all the car images.
 carBtn.addEventListener('click', () => showImages('car'));
@@ -13,25 +13,9 @@ carBtn.addEventListener('click', () => showImages('car'));
 // Displays all the nature images.
 natureBtn.addEventListener('click', () => showImages('nature'));
 
-function showImages(className) {
-    if (!className) return allImages.forEach(img => img.style.display = 'block');
-    allImages.forEach(img => {
-        if (img.className == className) img.style.display = 'block';
-        else img.style.display = 'none';
-    })
+function showImages(altName) {
+    images.forEach(img => img.style.display = (img.alt == altName) ? 'block' : 'none')
 }
 
-// Showing a pop when clicking the images.
-allImages.forEach(img => {
-    img.addEventListener('click', (e)=>{
-        modal.style.display = 'flex';
-        document.querySelector('.modal img').src  = e.target.src;
-    });
-});
 
-// closing the Popup.
-modal.addEventListener('click', (e) => {
-    const modalImg = modal.querySelector('img');
-    if(e.target != modalImg) modal.style.display = 'none';
-});
 
