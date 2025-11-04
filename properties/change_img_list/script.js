f452a190f25e7ec89739aef589d096ad77566fdc
Priymak Vladimir <PriymakVl@gmail.com>
2025-11-04 09:35:47 +0200

    change task change img list

--- properties/change_img_list/script.js
+++ properties/change_img_list/script.js
@@ -1,10 +1,23 @@
-const list = document.querySelector(".list");
+const catItem = document.querySelector("#cat");
+const dogItem = document.querySelector("#dog");
+const mouseItem = document.querySelector("#mouse");
 const img = document.querySelector('img');
 
-list.onclick = changeImg
+catItem.onclick = changeImg
+dogItem.onclick = changeImg
+mouseItem.onclick = changeImg
 
-function changeImg(event) {
-    let item = event.target;
-    img.src = item.getAttribute('data-src');
-    // img.src = item.dataset.src;
+function changeImg() {
+    let file = this.getAttribute('data-file');
+    // Вариант получения значения нестандартного атрибута
+    // let file = this.dataset.file;
+    img.src = 'img/' + file;
+    setActiveColor(this);
+}
+
+function setActiveColor(activeItem) {
+    catItem.style.color = 'black';
+    dogItem.style.color = 'black';
+    mouseItem.style.color = 'black';
+    activeItem.style.color = 'red';
 }
